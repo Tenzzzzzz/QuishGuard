6fd01bfa080bf98256390bdc897d47483ebc1eb3
yosef khaled shehata <124172634+Tenzzzzzz@users.noreply.github.com>
2026-02-09 20:26:26 +0200

    Edited client

--- client.py
+++ client.py
@@ -4,5 +4,4 @@ Example usage:
 with open("email_file.eml", "rb") as f:
     files={"file": f}
     response = requests.post("http://127.0.0.1:5001/submit",files=files)
-    print(response.text)
 """
